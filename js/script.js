4b945a0a0e328b33c5f33286f861dc467821a4a6
Travis Williams <traviswilliams997@gmail.com>
2022-05-30 18:37:45 -0500

    Fix operate function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,26 @@
 function add(num1, num2){
+    //console.log(num1 + num2);
     return num1 + num2;
 }
 
 function subtract(num1, num2){
+    //console.log(num1 - num2);
     return num1 - num2;
 }
 
 function multiply(num1, num2){
+    //console.log(num1 * num2);
     return num1 * num2;
 }
 
 function divide(num1, num2){
+    //console.log(num1 / num2);
     return num1 / num2;
     
 }
 
 function operate(operator, num1, num2){
+
     
     if(operator == '+'){
         add(num1, num2);
@@ -27,7 +32,9 @@ function operate(operator, num1, num2){
         multiply(num1, num2);
     }
     if(operator == '/'){
-        add(num1, num2);
+        divide(num1, num2);
     }
     
 }
+
+
