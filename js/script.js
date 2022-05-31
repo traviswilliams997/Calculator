1376e0d7aac043d8e857a3d8b596d9949f2d695d
Travis Williams <traviswilliams997@gmail.com>
2022-05-31 02:45:06 -0500

    Remove console logs

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
 function add(num1, num2){
-    //console.log(num1 + num2);
     return num1 + num2;
 }
-
 function subtract(num1, num2){
-    //console.log(num1 - num2);
     return num1  - num2;
 }
-
 function multiply(num1, num2){
-    //console.log(num1 * num2);
     return num1 * num2;
 }
-
 function divide(num1, num2){
-    //console.log(num1 / num2);
     return num1 / num2;
-    
 }
 
 function operate(operator, num1, num2){
-
-    
     if(operator == '+'){
         return add(num1, num2);
     }
@@ -43,7 +33,6 @@ const displayContent = document.createElement('div');
 displayContent.classList.add('displayContent');
 let displayValue = "";
 
-
 function calcDisplay(value, displayContent, display){
     displayContent.textContent = value; 
     display.appendChild(displayContent);
@@ -52,11 +41,9 @@ function calcDisplay(value, displayContent, display){
 
 //To display multiple digit numbers
 function play(i){
-
     displayValue += i;
     calcDisplay(displayValue, displayContent, display);
     lastValue = parseInt(displayValue);
-   
 }
 
 const digits = document.querySelectorAll('.digits');
@@ -64,127 +51,90 @@ for (let i = 0; i < digits.length; i++) {
     digits[i].addEventListener('click', function(){play(i)});
     }
 
-
-
 let lastValue = 0;
 let firstNum = 0;
 let output = 0 ;
-
 let addPush = 'no';
 let subPush = 'no';
 let mulPush ='no';
 let divPush = 'no';
 let equalPush = 'no';
 
-
-
-
 //Addition button
-
 const addbtn = document.querySelector('#add');
-    addbtn.addEventListener('click', () => {
-        displayValue = "";
-
-      
-        if(addPush == 'yes'){
-            output =  operate('+', firstNum, lastValue);
-            console.log('add push at add ' + output);
-            calcDisplay(output, displayContent, display);     
-            firstNum = output;
-        
-        }else{
-          
-            if(subPush == 'no' && mulPush == 'no' && divPush == 'no' && equalPush == 'no'){
-                firstNum = lastValue;
-               }
-                addPush = 'yes';
+addbtn.addEventListener('click', () => {
+    displayValue = "";
+    if(addPush == 'yes'){
+        output =  operate('+', firstNum, lastValue);
+        calcDisplay(output, displayContent, display);     
+        firstNum = output;
+    }else{
+        if(subPush == 'no' && mulPush == 'no' && divPush == 'no' && equalPush == 'no'){
+            firstNum = lastValue;
         }
-
-        
-        whichPushed(0, subPush, mulPush, divPush);
-       
-             
-        console.log('at add button: push equal ' + addPush);
+        addPush = 'yes';
+    }   
+        whichPushed(0, subPush, mulPush, divPush);        
       });
 
 //Subtraction button
 const subbtn = document.querySelector('#subtract');
 subbtn.addEventListener('click', () => {
     displayValue = "";
-    
-        if(subPush == 'yes' ){
-            output =  operate('-', firstNum, lastValue);
-            console.log('sub push at sub' + output);
-            calcDisplay(output, displayContent, display);
-           
-            firstNum = output;
-           
-           }else{
-               if(addPush == 'no' && mulPush == 'no' && divPush == 'no' && equalPush == 'no'){
+    if(subPush == 'yes' ){
+        output =  operate('-', firstNum, lastValue);
+        calcDisplay(output, displayContent, display);
+        firstNum = output;
+    }else{
+        if(addPush == 'no' && mulPush == 'no' && divPush == 'no' && equalPush == 'no'){
              firstNum = lastValue;
-            }
-             subPush = 'yes';
-           }
-           whichPushed(addPush, 0, mulPush, divPush);
-       
-        console.log('at sub button: push equal ' + subPush);
+        }
+        subPush = 'yes';
+    }
+    whichPushed(addPush, 0, mulPush, divPush);       
       });
     
 //Multiply button
 const mulbtn = document.querySelector('#multiply');
 mulbtn.addEventListener('click', () => {
     displayValue = "";
-        if(mulPush == 'yes' ){
-            output =  operate('*', firstNum, lastValue);
-            console.log('mul push at mul' + output);
-            calcDisplay(output, displayContent, display);
-           
-            firstNum = output;
-           
-           }else{
-               if(addPush == 'no' && subPush == 'no' && divPush == 'no' && equalPush == 'no'){
+    if(mulPush == 'yes' ){
+        output =  operate('*', firstNum, lastValue);
+        calcDisplay(output, displayContent, display);   
+        firstNum = output;   
+    }else{
+        if(addPush == 'no' && subPush == 'no' && divPush == 'no' && equalPush == 'no'){
              firstNum = lastValue;
             }
-             mulPush = 'yes';
-           }
-           whichPushed(addPush, subPush, 0, divPush);
-       
-        console.log('at mul button: push equal ' + mulPush);
+        mulPush = 'yes';
+    }
+    whichPushed(addPush, subPush, 0, divPush);       
       });
 
 //Division button
 const divbtn = document.querySelector('#divide');
 divbtn.addEventListener('click', () => {
     displayValue = "";
-        if(divPush == 'yes' ){
-            output =  operate('/', firstNum, lastValue);
-            console.log('div push at div ' + output);
-            calcDisplay(output, displayContent, display);
-           
-            firstNum = output;
-           
-           }else{
-               if(addPush == 'no' && subPush == 'no' && mulPush == 'no' && equalPush == 'no'){
-             firstNum = lastValue;
-            }
-             divPush = 'yes';
-           }
-           whichPushed(addPush, subPush, mulPush, 0);
-         
-       
-        console.log('at div button: push equal ' + divPush);
+    if(divPush == 'yes' ){
+        output =  operate('/', firstNum, lastValue);
+        calcDisplay(output, displayContent, display);
+        firstNum = output;   
+    }else{
+        if(addPush == 'no' && subPush == 'no' && mulPush == 'no' && equalPush == 'no'){
+        firstNum = lastValue;
+        }
+        divPush = 'yes';
+     }
+    whichPushed(addPush, subPush, mulPush, 0);       
       });
 
 //Equals button
 const equalbtn = document.querySelector('#equal');
 equalbtn.addEventListener('click', () => {
     displayValue = "";
-
     whichPushed(addPush, subPush, mulPush, divPush);
-
-           equalPush =  'yes';
+    equalPush = 'yes';
         
-        console.log('at add button: push equal ' + equalPush);
       });
 
 const clearbtn = document.querySelector('#clear');
@@ -199,59 +149,36 @@ clearbtn.addEventListener('click', () => {
     divPush = 'no';
     equalPush =  'no';
     calcDisplay(displayValue, displayContent, display);
-              
-             
             });
       
-
-
-
 //Repeated calculations
 function whichPushed(addP, subP, mulP, divP){
-    
-    if(addP == 'yes'){
-       
+    if(addP == 'yes'){    
      output =  operate('+', firstNum, lastValue);
-     console.log('add push at which' + output);
      calcDisplay(output, displayContent, display);
-    
      firstNum = output;
      addPush ='no';
-    
     }
     if(subP == 'yes'){
-       
         output =  operate('-', firstNum, lastValue);
-        console.log('sub  push at which' + output);
         calcDisplay(output, displayContent, display);
-       
         firstNum = output;
         subPush ='no';
-       
-       }
-       if(mulP == 'yes'){
-       
+    }
+    if(mulP == 'yes'){
         output =  operate('*', firstNum, lastValue);
         console.log('mul  push at which' + output);
         calcDisplay(output, displayContent, display);
-       
         firstNum = output;
         mulPush ='no';
-       
-       }
-       if(divP == 'yes'){
-           
+    }
+    if(divP == 'yes'){
         output =  operate('/', firstNum, lastValue);
         console.log('div  push at which' + output);
         calcDisplay(output, displayContent, display);
-       
         firstNum = output;
         divPush ='no';
        
-       }
-     
-   
-    console.log('which pressed' + firstNum);
-    
+    }
 }
 
