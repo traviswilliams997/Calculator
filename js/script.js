188da2c2c73a9f81cdff8fd02d27d5e8cec68b85
Travis Williams <traviswilliams997@gmail.com>
2022-06-01 06:53:22 -0500

    Limit decimal places to 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
 function add(num1, num2){
-    return num1 + num2;
+    let ans =  num1 + num2;
+    ans = ans.toFixed(2);
+    return ans;
 }
 function subtract(num1, num2){
-    return num1  - num2;
+    let ans =  num1 - num2;
+    ans = ans.toFixed(2);
+    return ans;
 }
 function multiply(num1, num2){
-    return num1 * num2;
+    let ans =  num1 * num2;
+    ans =  ans.toFixed(2);
+    return ans;
 }
 function divide(num1, num2){
     if(num2 !== 0 ){
-    return num1 / num2;
+        let ans =  num1 / num2;
+        ans = ans.toFixed(2);
+        return ans;
     }else{
         return 0;
     }
